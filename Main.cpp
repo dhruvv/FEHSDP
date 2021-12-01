d7cfac6e2663c3a41d0f1ed1b7f4d26158d9da8d
dhruvv <dhruva.venkataraman@gmail.com>
2021-12-01 16:23:17 -0500

    Adding rect

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,11 @@
-#include <FEHUtils.h>
-#include <FEHIO.h>
+#include <FEHUtility.h>
+//#include <FEHIO.h>
 #include <FEHLCD.h>
+
+
+
+
+int main(){
+    LCD.SetFontColor(WHITE);
+    LCD.DrawRectangle(10, 10, 100, 100 );
+}
